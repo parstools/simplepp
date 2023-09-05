67714ab2fa09068dfd99283bf113221e83ec4585
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-09-05 16:29:13 +0200

    File class not nested

--- FileStack.cpp
+++ FileStack.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <utility>
+#include <iostream>
 #include "FileStack.h"
 
 using namespace std;
@@ -9,18 +10,55 @@ void FileStack::preprocess(std::string filename) {
     stack[0].read(std::move(filename));
     while(stack.size()>0) {
         auto & top = stack.back();
+        if (top.isDirective()) {
+            auto dt = top.directiveType();
+            switch(dt) {
+                case File::dtInclude:
+                    //stack.emplace_back();
+                    //stack.back().read(top.askInclude());
+                    break;
+               }
+        }
+        else
+           cout << top.currLine() << endl;
         top.currentLine++;
         if (top.eof())
             stack.pop_back();
     }
 }
 
-bool FileStack::File::isDirective(std::string line) {
-    line = trimLeft(line);
-    return !line.empty() and line[0]=='#';
+
+std::string File::currLine() {
+    return lines[currentLine];
 }
 
-string FileStack::File::trimLeft(const string& str)
+bool File::isDirective() {
+    std::string line = currLine();
+    line = trimLeft(line);
+    return !line.empty() and line[0] == '#';
+}
+
+File::DirectiveType File::directiveType() {
+    std::string line = currLine();
+    line = trimLeft(line);
+    if (line.empty() || line[0] != '#')
+        return dtNone;
+    size_t pos = 1;
+    while (isalnum(line[pos])) pos++;
+    string word = line.substr(1,pos-1);
+    if (word=="include") return dtInclude;
+    else if (word=="define") return dtDefine;
+    else if (word=="ifdef") return dtIfdef;
+    else if (word=="ifndef") return dtIfndef;
+    else if (word=="endif") return dtEndif;
+    else if (word=="else") return dtElse;
+    else if (word=="if") return dtIf;
+    else {
+        throw runtime_error("bad directive: "+line);
+    }
+}
+
+string File::trimLeft(const string& str)
 {
     const auto strBegin = str.find_first_not_of(" \t");
     if (strBegin == string::npos)
@@ -28,7 +66,7 @@ string FileStack::File::trimLeft(const string& str)
     return str.substr(strBegin, str.length() - strBegin);
 }
 
-string FileStack::File::trimRight(const string& str)
+string File::trimRight(const string& str)
 {
     const auto strEnd = str.find_last_not_of(" \t\r");
     if (strEnd == string::npos)
@@ -36,11 +74,11 @@ string FileStack::File::trimRight(const string& str)
     return str.substr(0, strEnd + 1);
 }
 
-string FileStack::File::trim(const string& str) {
+string File::trim(const string& str) {
     return trimLeft(trimRight(str));
 }
 
-void FileStack::File::read(std::string filename) {
+void File::read(std::string filename) {
     canonicalPath = std::filesystem::canonical(filename);
     std::ifstream infile(canonicalPath);
     lines.clear();
@@ -48,6 +86,7 @@ void FileStack::File::read(std::string filename) {
         lines.push_back(trimRight(line));
 }
 
-bool FileStack::File::eof() {
+bool File::eof() {
     return currentLine>=lines.size();
 }
+
--- FileStack.h
+++ FileStack.h
@@ -10,19 +10,25 @@
 #include <filesystem>
 #include <vector>
 
+class File {
+    static std::string trimLeft(const std::string& str);
+    static std::string trimRight(const std::string& str);
+    static std::string trim(const std::string& str);
+public:
+    enum DirectiveType {dtNone, dtInclude, dtDefine, dtIfdef, dtIfndef, dtEndif,
+        dtElse, dtIf};
+    bool isDirective();
+    DirectiveType directiveType();
+    std::string currLine();
+    std::filesystem::path canonicalPath;
+    std::vector<std::string> lines;
+    int currentLine = 0;
+    void read(std::string filename);
+    bool eof();
+};
+
 class FileStack {
     std::unordered_map<std::string,std::string> variables;
-    struct File {
-        static bool isDirective(std::string line);
-        std::filesystem::path canonicalPath;
-        std::vector<std::string> lines;
-        int currentLine = 0;
-        void read(std::string filename);
-        static std::string trimLeft(const std::string& str);
-        static std::string trimRight(const std::string& str);
-        static std::string trim(const std::string& str);
-        bool eof();
-    };
     std::vector<File> stack;
 public:
     void preprocess(std::string filename);
